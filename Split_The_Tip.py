42debb2fd73e3c0d176184b9e61305740f4f91fb
kin018 <60412598+kin018@users.noreply.github.com>
2022-02-24 19:31:17 -0500

    Update Split_The_Tip.py

--- Split_The_Tip.py
+++ Split_The_Tip.py
@@ -1,5 +1,9 @@
 meal=float(input("Enter dinner amount?: "))
+
 guests=int(input("Enter number of guest:"))
+
 tip= 1.15 #Tip amount...In this example we use 15%
+
 total_cost= (meal/guests) * tip
+
 print("With Tip, you each pay:$ %.2f" % total_cost)
